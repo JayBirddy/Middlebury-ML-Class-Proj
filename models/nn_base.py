772279936def91664d6500ee6120e5194ea9eb13
Jayden Chen <jjchen@middlebury.edu>
2026-05-13 19:01:49 -0400

    file and directory adjustments, minor additions to report

--- models/nn_base.py
+++ models/nn_base.py
@@ -14,34 +14,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch.nn as nn
 
-class ReadmissionMLP(nn.Module):
-    """
-    3-hidden-layer MLP for binary readmission prediction.
-    """
-    def __init__(self, input_dim):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(input_dim, 128),
-            nn.BatchNorm1d(128),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-
-            nn.Linear(128, 64),
-            nn.BatchNorm1d(64),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-
-            nn.Linear(64, 32),
-            nn.BatchNorm1d(32),
-            nn.ReLU(),
-            nn.Dropout(0.2),
-
-            nn.Linear(32, 1),
-        )
-
-    def forward(self, x):
-        return self.net(x).squeeze(1)
-
 df = pd.read_csv("data/diabetic_data.csv")
 df.replace("?", pd.NA, inplace=True)
 df.dropna(axis=1, thresh=int(0.6 * len(df)), inplace=True)
